b3f4353a68dca4207b31705f82eb2e2701f08ad9
Zak Thompson <zak@thisfireinside.com>
2013-11-09 22:03:47 -0500

    [Zak] Fixed segfault on fork

--- os161-1.99/kern/syscall/fork_syscalls.c
+++ os161-1.99/kern/syscall/fork_syscalls.c
@@ -13,6 +13,7 @@
 
 #if OPT_A2
 #include <addrspace.h>
+#include <spl.h>
 
 /*
   Duplicates the currently running process
@@ -20,20 +21,32 @@
 */
 int
 sys_fork(struct trapframe *tf, int32_t *retval) {
+
+  // Prevent interrupts so address space doesn't change before getting
+  // copied
+  int spl = splhigh();
+
   // Store trapframe on the heap for copying to child
   // thread later
   struct trapframe *child_tf;
   child_tf = kmalloc(sizeof(tf));
-  if (child_tf == NULL) return ENOMEM;
+  if (child_tf == NULL) {
+    splx(spl);
+    return ENOMEM;
+  }
 
-  memcpy(child_tf, tf, sizeof(tf));
+  //memcpy(child_tf, tf, sizeof(tf));
+  child_tf = tf;
 
   // Copy parent address space
   struct addrspace *original_as;
   struct addrspace *new_as;
   original_as = curproc_getas();
   int result = as_copy(original_as, &new_as);
-  if (result) return ENOMEM;
+  if (result) {
+    splx(spl);
+    return ENOMEM;
+  }
 
   // Create new process
   struct proc *new_proc = proc_create(curproc->p_name);
@@ -42,16 +55,17 @@ sys_fork(struct trapframe *tf, int32_t *retval) {
   result = thread_fork("Child process", new_proc, enter_forked_process, (void *)child_tf, (unsigned long)new_as);
   if (result) {
     kfree(child_tf);
+    splx(spl);
     return result;  // Error code will be returned from thread_fork
   }
-  
+
   //Child process needs to have parent pid attached to it
   set_parent(curproc->pid, new_proc->pid);
 
 
   // Set retval to child process pid
   *retval = new_proc->pid;
-
+  splx(spl);
   return 0;
 }
 #endif
