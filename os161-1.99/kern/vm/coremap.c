65d7b75de289f68a1b7ad6c17efbd7c25bf05ef8
Zak Thompson <zak@thisfireinside.com>
2013-12-01 01:33:58 -0500

    [Zak] Now compiles

--- os161-1.99/kern/vm/coremap.c
+++ os161-1.99/kern/vm/coremap.c
@@ -2,8 +2,9 @@
 #include <types.h>
 #include <lib.h>
 #include <vm.h>
+#include <pt.h>
 
-struct pte ** cm_pages;
+struct pte * cm_pages;
 
 void coremap_init(paddr_t first, paddr_t last){
 	__u32 total_pages = (last + PAGE_SIZE-1)/PAGE_SIZE;
@@ -12,27 +13,13 @@ void coremap_init(paddr_t first, paddr_t last){
 	if(PADDR_TO_KVADDR(first) == 0){
 		panic("Physical address is 0. Oh noes");
 	}
-	cm_pages = (struct pte**)PADDR_TO_KVADDR(first);
+	cm_pages = (struct pte*)PADDR_TO_KVADDR(first);
 	//paddr_t kernel_addr = first + pages * sizeof(struct pte);
 
-	struct pte ** pages_iter = cm_pages;
-	paddr_t address_iter = first;
-	size_t size = sizeof(**pages_iter);
-
-	for(__u32 i =0; i < kernel_pages; i++){
-		*pages_iter->paddr = address_iter;
-		*pages_iter->valid = 1;
-		*pages_iter->dirty = 0;
-		pages_iter++;
-		address_iter+=size;
-	}
-
 	for(__u32 i =0; i < free_pages; i++){
-		*pages_iter->paddr = address_iter;
-		*pages_iter->valid = 1;
-		*pages_iter->dirty = 1;
-		pages_iter++;
-		address_iter+=size;
+		cm_pages[i].paddr = kernel_pages + PAGE_SIZE * i;
+		cm_pages[i].valid = 1;
+		cm_pages[i].dirty = 1;
 	}
 }
 
