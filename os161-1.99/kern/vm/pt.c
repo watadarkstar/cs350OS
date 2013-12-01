5e1fd27074f0e6da2b3f3de81ba9eae0c396f9cf
Zak Thompson <zak@thisfireinside.com>
2013-12-01 00:09:40 -0500

    [Zak] Added TLB reload tracker

--- os161-1.99/kern/vm/pt.c
+++ os161-1.99/kern/vm/pt.c
@@ -2,6 +2,7 @@
 #include <lib.h>
 #include <addrspace.h>
 #include <pt.h>
+#include <uw-vmstats.h>
 
 void
 pte_create(struct pte *pte) {
@@ -17,6 +18,7 @@ paddr_t pte_load(struct pte *pte){
 }
 
 paddr_t pte_reload(struct pte *pte, int offset){
+  vmstats_inc(VMSTAT_TLB_RELOAD);
 	/* Translate the address based on the offset */
 	paddr_t paddr = pte->paddr + offset;
 	return paddr;
